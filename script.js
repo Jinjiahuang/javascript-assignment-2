915983235e83f35737cc933a408a3b53ac397e28
Jinjia Huang <jhuang64@id.iit.edu>
2017-04-13 22:05:49 -0500

    no message

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Do your work in this file.
 
 var boxes = document.querySelectorAll(".box");
 var a = boxes.length;
@@ -43,40 +42,3 @@ for (var i = 0; i < a; i++) {
         })(i)  
 }
 
-
-// var boxes = document.querySelectorAll(".box");
-// var a = boxes.length;
-// var color;
-
-// if (201 <= a && a <= 300){
-//     color = "yellow";
-// }
-// else if (301 <= a && a <= 400){
-//     color = "green";
-// }
-// else if (401 <= a && a <= 500){
-//     color = "purple";
-// }
-// else if (501 <= a && a <= 600){
-//     color = "orange";
-// }
-// else if (601 <= a && a <= 700){
-//     color = "pink";
-// }
-// else if (701 <= a && a <= 800){
-//     color = "black";
-// }
-// else if (801 <= a && a <= 900){
-//     color = "brown";
-// }
-// else {
-//     color = "blue";
-// }
-
-// for (var i = 0; i < a; i++) {
-//     boxes[i].addEventListener ("click",boxClicked);
-// }
-
-// function boxClicked (){
-
-// }
